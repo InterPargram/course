dd0d73a94aac032755ffc522e6083b1800b29602
gaojinming <2712225255@qq.com>
2019-10-09 20:30:32 +0800

    更新来关于数据库连接池的相关代码

--- JDBC/src/JDBCTools.java
+++ JDBC/src/JDBCTools.java
@@ -1,8 +1,10 @@
-<<<<<<< HEAD
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
 import org.junit.Test;
 
-=======
->>>>>>> b577d1ba1c44c828a76ff0ab337f114bcf5b809f
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -20,33 +22,30 @@ import java.util.Properties;
  */
 public class JDBCTools {
 
-<<<<<<< HEAD
 
     /*
      * 加强版updata 使用PreparedStatement
      * 执行 sql ; obj 是占位符的可变参数
      * */
     public static void updata(String sql, Object... obj) {
-        Connection connection=null;
-        PreparedStatement ps=null;
-        try{
-            connection=JDBCTools.getConnection();
-            ps=connection.prepareStatement(sql);
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = JDBCTools.getConnection();
+            ps = connection.prepareStatement(sql);
 
-            for (int i=0;i<obj.length;i++){
-                ps.setObject(i+1,obj[i]);
+            for (int i = 0; i < obj.length; i++) {
+                ps.setObject(i + 1, obj[i]);
             }
 
             ps.executeUpdate();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            JDBCTools.release(ps,connection);
+        } finally {
+            JDBCTools.release(ps, connection);
         }
     }
 
-=======
->>>>>>> b577d1ba1c44c828a76ff0ab337f114bcf5b809f
     /*
      * 用来执行 sql 语句的方法 insert delete updata
      * */
@@ -75,11 +74,7 @@ public class JDBCTools {
     }
 
     public static void release(ResultSet rs, Statement statement, Connection connection) {
-<<<<<<< HEAD
         if (rs != null) {
-=======
-        if (rs!=null){
->>>>>>> b577d1ba1c44c828a76ff0ab337f114bcf5b809f
             try {
                 rs.close();
             } catch (SQLException e) {
@@ -95,6 +90,8 @@ public class JDBCTools {
         }
         if (connection != null) {
             try {
+                //数据库连接池中的Connection对象进行close时并不是将连接进行关闭
+                //而是将数据库连接换回到连接池中
                 connection.close();
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -119,28 +116,16 @@ public class JDBCTools {
         }
     }
 
+    //数据库连接池应只被初始化一次
+    private static DataSource dataSource = null;
+
+    static {
+        dataSource = new ComboPooledDataSource("helloc3p0");
+
+    }
+
     public static Connection getConnection() throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException, IOException {
-        String driverClass = null;
-        String jdbcUrl = null;
-        String user = null;
-        String password = null;
 
-        //读取类文件下的jdbc.properties文件
-        InputStream in = JDBCTools.class.getClassLoader().getResourceAsStream("jdbc.properties");
-        Properties properties = new Properties();
-        properties.load(in);
-        driverClass = properties.getProperty("driver");
-        jdbcUrl = properties.getProperty("jdbcUrl");
-        user = properties.getProperty("user");
-        password = properties.getProperty("password");
-
-        Properties info = new Properties();
-        info.put("user", user);
-        info.put("password", password);
-        //通过反射创建 driver 对象
-        Driver driver = (Driver) Class.forName(driverClass).newInstance();
-        //通过 driver 的 connect 方法获取数据库的连接
-        Connection connection = driver.connect(jdbcUrl, info);
-        return connection;
+        return dataSource.getConnection();
     }
 }
